4189cdbbb3a5d6cab85d7f621058d0913b987b0f
Nicholas Dunnaway <nick@dunnaway.org>
2021-07-19 16:24:16 -0700

    File Import

---
+++ src/Digitalroot.Valheim.Common.AssemblyInfo/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Digitalroot.Valheim.Common
+{
+  public static class AssemblyInfo
+  {
+    public const string Title = "Digitalroot.Valheim.Common";
+    public const string Description = "Digitalroot Valheim Common";
+    public const string Configuration = "";
+    public const string Company = "Digitalroot Technologies";
+    public const string Product = "Digitalroot Valheim Mods";
+    public const string Copyright = "Copyright © Digitalroot Technologies 2021";
+    public const string Trademark = "";
+    public const string Culture = "";
+    public const string Version = "1.0.0";
+  }
+}

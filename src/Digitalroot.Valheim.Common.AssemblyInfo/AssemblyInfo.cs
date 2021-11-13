99c1b8e6f87617db32ba7158a479bc59c4c4502f
Nicholas Dunnaway <nick@dunnaway.org>
2021-11-12 22:54:24 -0800

    disable actions.

--- src/Digitalroot.Valheim.Common.AssemblyInfo/AssemblyInfo.cs
+++ src/Digitalroot.Valheim.Common.AssemblyInfo/AssemblyInfo.cs
@@ -11,7 +11,7 @@
     public const string Copyright = "Copyright © Digitalroot Technologies 2021";
     public const string Trademark = "";
     public const string Culture = "en-US";
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.0";
     public const string Author = "Digitalroot";
   }
 }

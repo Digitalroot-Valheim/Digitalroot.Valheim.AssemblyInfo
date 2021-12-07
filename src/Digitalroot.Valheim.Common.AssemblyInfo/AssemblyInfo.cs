9d40cf232916030b9c90f8caf7188a0f06069465
Digitalroot <Digitalroot@users.noreply.github.com>
2021-12-06 21:30:55 -0800

    Update AssemblyInfo.cs

--- src/Digitalroot.Valheim.Common.AssemblyInfo/AssemblyInfo.cs
+++ src/Digitalroot.Valheim.Common.AssemblyInfo/AssemblyInfo.cs
@@ -8,10 +8,10 @@
     public const string Configuration = "";
     public const string Company = "Digitalroot Technologies";
     public const string Product = "Digitalroot Valheim Mods";
-    public const string Copyright = "Copyright © Digitalroot Technologies 2021";
+    public const string Copyright = "Copyright © Digitalroot Technologies 2021 - 2022";
     public const string Trademark = "";
     public const string Culture = "en-US";
-    public const string Version = "1.0.0";
+    public const string Version = "0.0.1";
     public const string Author = "Digitalroot";
   }
 }
